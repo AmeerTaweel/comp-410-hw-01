0fd0750f18787a40a18482b7478bd03781e9e87a
Ameer Taweel <ameertaweel2002@gmail.com>
2023-03-20 21:55:22 +0300

    use glfw helpers in main

--- main.cpp
+++ main.cpp
@@ -6,26 +6,22 @@
 
 // GLFW
 // Handles windowing operations and keyboard/mouse events
-#include <GLFW/glfw3.h>
+#include "custom/glfw.cpp"
 
 // Use the standard library namespace by default
 using namespace std;
 
-int main() {
-	glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	// Required to work on macOS
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#define OPEN_GL_MAJOR_VERSION 3
+#define OPEN_GL_MINOR_VERSION 3
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-	if (window == NULL)
-	{
-		    std::cout << "Failed to create GLFW window" << std::endl;
-			    glfwTerminate();
-				    return -1;
-	}
+#define WINDOW_WIDTH  800
+#define WINDOW_HEIGHT 600
+#define WINDOW_TITLE  "ZERO NO GATO"
+
+int main() {
+	custom::glfw_init(OPEN_GL_MAJOR_VERSION, OPEN_GL_MINOR_VERSION);
+
+	auto window = custom::glfw_create_window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 	glfwMakeContextCurrent(window);
 
 	// Load OpenGL function pointers
