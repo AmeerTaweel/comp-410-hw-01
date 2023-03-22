29c8904e8d259e899cfb9b034d6538443621781c
Ameer Taweel <ameertaweel2002@gmail.com>
2023-03-22 22:40:41 +0300

    use model utils

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@
 // Compile / Link OpenGL Shaders
 #include "custom/gl_shader.cpp"
 
+// Load Models / Model Utils
+#include "custom/model.cpp"
+
 // GLFW
 // Handles windowing operations and keyboard/mouse events
 #include "custom/glfw.cpp"
@@ -23,6 +26,11 @@ using namespace std;
 #define WINDOW_WIDTH  800
 #define WINDOW_HEIGHT 600
 #define WINDOW_TITLE  "ZERO NO GATO"
+
+/* template <class T> */
+/* inline void glBufferData(GLenum target, const vector<T>& v, GLenum usage) { */
+/* 	    glBufferData(target, v.size() * sizeof(T), &v[0], usage); */
+/* } */
 
 int main() {
 	// Initialize GLFW
@@ -47,16 +55,7 @@ int main() {
 	auto program = custom::gl_make_program("vertex_shader.glsl", "fragment_shader.glsl");
 	glUseProgram(program);
 
-	float vertices[] = {
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
-	};
-	unsigned int indices[] = {  // note that we start from 0!
-		0, 1, 3,   // first triangle
-		1, 2, 3    // second triangle
-	};
+	auto model = custom::model_sg0_load("bunny.tlst");
 
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);  
@@ -72,15 +71,19 @@ int main() {
 	glBindVertexArray(VAO);
 	// 2. copy our vertices array in a vertex buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	/* glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); */
+	/* glBufferData(GL_ARRAY_BUFFER, *(model->vertices), GL_STATIC_DRAW); */
+	glBufferData(GL_ARRAY_BUFFER, model->vertices->size() * sizeof(float), &model->vertices->front(), GL_STATIC_DRAW);
 	// 3. copy our index array in a element buffer for OpenGL to use
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	/* glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); */
+	/* glBufferData(GL_ARRAY_BUFFER, *(model->triangles), GL_STATIC_DRAW); */
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, model->triangles->size() * sizeof(int), &model->triangles->front(), GL_STATIC_DRAW);
 	// 4. then set the vertex attributes pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	/* glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); */
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	// Render loop
@@ -89,7 +92,7 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, model->triangle_count * 3, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
