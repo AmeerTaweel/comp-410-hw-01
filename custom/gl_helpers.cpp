1ce2c30d113240bf55e09764657b1d41796ea598
Ameer Taweel <ameertaweel2002@gmail.com>
2023-03-26 15:10:10 +0300

    refactor helpers

--- custom/gl_helpers.cpp
+++ custom/gl_helpers.cpp
@@ -8,6 +8,8 @@
 namespace custom {
 	using namespace std;
 
+	// Move vectors to GPU buffer
+	// Standard glBufferData accepst only arrays
 	template <class T> void glBufferDataV(GLenum target, const vector<T> v, GLenum usage) {
 		glBufferData(target, v.size() * sizeof(T), &v.front(), usage);
 	}
